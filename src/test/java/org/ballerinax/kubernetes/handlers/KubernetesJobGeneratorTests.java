cb3647cb47186525d3dfced8daeb9fa2d0d5a330
anuruddhal <anuruddhal@wso2.com>
2018-04-23 11:33:58 +0530

    Add unit tests

---
+++ src/test/java/org/ballerinax/kubernetes/handlers/KubernetesJobGeneratorTests.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinax.kubernetes.handlers;
+
+import io.fabric8.kubernetes.api.KubernetesHelper;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.Job;
+import org.ballerinax.kubernetes.KubernetesConstants;
+import org.ballerinax.kubernetes.exceptions.KubernetesPluginException;
+import org.ballerinax.kubernetes.models.JobModel;
+import org.ballerinax.kubernetes.utils.KubernetesUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Test job generation.
+ */
+public class KubernetesJobGeneratorTests {
+
+    private final Logger log = LoggerFactory.getLogger(KubernetesJobGeneratorTests.class);
+    private final String jobName = "MyJOB";
+    private final String selector = "TestAPP";
+    private final String imageName = "SampleImage:v1.0.0";
+    private final String imagePullPolicy = "Always";
+    private final int replicas = 5;
+
+
+    @Test
+    public void testServiceGenerate() {
+        JobModel jobModel = new JobModel();
+        jobModel.setName(jobName);
+        Map<String, String> labels = new HashMap<>();
+        labels.put(KubernetesConstants.KUBERNETES_SELECTOR_KEY, selector);
+        jobModel.setLabels(labels);
+        jobModel.setImage(imageName);
+        jobModel.setImagePullPolicy(imagePullPolicy);
+        HashMap<String, String> env = new HashMap<>();
+        env.put("ENV_VAR", "ENV");
+        jobModel.setEnv(env);
+
+        try {
+            String jobYAML = new JobHandler(jobModel).generate();
+            Assert.assertNotNull(jobYAML);
+            File artifactLocation = new File("target/kubernetes");
+            artifactLocation.mkdir();
+            File tempFile = File.createTempFile("temp", jobModel.getName() + ".yaml", artifactLocation);
+            KubernetesUtils.writeToFile(jobYAML, tempFile.getPath());
+            log.info("Generated YAML: \n" + jobYAML);
+            Assert.assertTrue(tempFile.exists());
+            testGeneratedYAML(tempFile);
+            tempFile.deleteOnExit();
+        } catch (IOException e) {
+            Assert.fail("Unable to write to file");
+        } catch (KubernetesPluginException e) {
+            Assert.fail("Unable to generate yaml from service");
+        }
+    }
+
+    private void testGeneratedYAML(File yamlFile) throws IOException {
+        Job job = KubernetesHelper.loadYaml(yamlFile);
+        Assert.assertEquals(jobName, job.getMetadata().getName());
+        Assert.assertEquals(1, job.getSpec().getTemplate().getSpec().getContainers().size());
+        Container container = job.getSpec().getTemplate().getSpec().getContainers().get(0);
+        Assert.assertEquals(imageName, container.getImage());
+        Assert.assertEquals(imagePullPolicy, container.getImagePullPolicy());
+        Assert.assertEquals(1, container.getEnv().size());
+    }
+}
