9bad64986c8e1bbe1c4c6d0c5916697d4cfca247
Hemika Kodikara <hemika.kodikara@gmail.com>
2018-11-24 15:56:17 +0530

    Updated to the latest syntax

--- kubernetes-extension/src/main/java/org/ballerinax/kubernetes/processors/ServiceAnnotationProcessor.java
+++ kubernetes-extension/src/main/java/org/ballerinax/kubernetes/processors/ServiceAnnotationProcessor.java
@@ -50,7 +50,7 @@ public class ServiceAnnotationProcessor extends AbstractAnnotationProcessor {
         BLangService bService = (BLangService) serviceNode;
         if (!(bService.attachExpr instanceof BLangTypeInit)) {
             throw new KubernetesPluginException("Adding @kubernetes:Service{} annotation to a service is only " +
-                    "supported when the has an anonymous listener");
+                    "supported when it has an anonymous listener");
         }
         ServiceModel serviceModel = getServiceModelFromAnnotation(attachmentNode);
         if (isBlank(serviceModel.getName())) {
