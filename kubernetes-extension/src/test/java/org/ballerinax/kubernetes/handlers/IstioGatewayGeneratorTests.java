4b3591880d6c8e2437e834e4b338324ada0b3a41
Hemika Kodikara <hemika.kodikara@gmail.com>
2019-03-13 13:29:26 +0530

    use snowdrop models for istio gateway tests

--- kubernetes-extension/src/test/java/org/ballerinax/kubernetes/handlers/IstioGatewayGeneratorTests.java
+++ kubernetes-extension/src/test/java/org/ballerinax/kubernetes/handlers/IstioGatewayGeneratorTests.java
@@ -18,7 +18,7 @@
 
 package org.ballerinax.kubernetes.handlers;
 
-import org.apache.commons.io.FileUtils;
+import me.snowdrop.istio.api.networking.v1alpha3.Gateway;
 import org.ballerinax.kubernetes.KubernetesConstants;
 import org.ballerinax.kubernetes.exceptions.KubernetesPluginException;
 import org.ballerinax.kubernetes.handlers.istio.IstioGatewayHandler;
@@ -26,12 +26,13 @@ import org.ballerinax.kubernetes.models.KubernetesContext;
 import org.ballerinax.kubernetes.models.istio.IstioGatewayModel;
 import org.ballerinax.kubernetes.models.istio.IstioPortModel;
 import org.ballerinax.kubernetes.models.istio.IstioServerModel;
+import org.ballerinax.kubernetes.utils.Utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -80,36 +81,30 @@ public class IstioGatewayGeneratorTests {
         KubernetesContext.getInstance().getDataHolder().addIstioGatewayModel("sample-svc", istioGatewayModel);
         try {
             new IstioGatewayHandler().createArtifacts();
-            File yamlFile = new File("target" + File.separator + "kubernetes" + File.separator +
-                                     "hello" + ISTIO_GATEWAY_FILE_POSTFIX + YAML);
-            Yaml yaml = new Yaml();
-            Map gateway = (LinkedHashMap) yaml.load(FileUtils.readFileToString(yamlFile));
-    
+            File gwYaml = Paths.get("target", "kubernetes", "hello" + ISTIO_GATEWAY_FILE_POSTFIX + YAML).toFile();
+            Gateway gateway = Utils.loadYaml(gwYaml);
+            
             // metadata
-            Map metadata = (Map) gateway.get("metadata");
-            Assert.assertEquals("my-gateway", metadata.get("name"));
+            Assert.assertNotNull(gateway.getMetadata());
+            Assert.assertEquals(gateway.getMetadata().getName(), "my-gateway");
     
-            Map spec = (Map) gateway.get("spec");
-            
-            Map selector = (Map) spec.get("selector");
-            Assert.assertEquals("my-gatweway-controller", selector.get(KubernetesConstants.KUBERNETES_SELECTOR_KEY));
+            Assert.assertNotNull(gateway.getSpec());
+            Assert.assertEquals(gateway.getSpec().getSelector().get(KubernetesConstants.KUBERNETES_SELECTOR_KEY),
+                    "my-gatweway-controller");
     
-            List servers = (List) spec.get("servers");
-            Map server = (Map) servers.get(0);
-            
-            Map port = (Map) server.get("port");
-            Assert.assertEquals(80, port.get("number"));
-            Assert.assertEquals("HTTP", port.get("protocol"));
-            Assert.assertEquals("http", port.get("name"));
+            Assert.assertEquals(gateway.getSpec().getServers().size(), 1);
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getPort().getNumber().intValue(), 80);
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getPort().getProtocol(), "HTTP");
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getPort().getName(), "http");
     
-            List<String> hosts = (List<String>) server.get("hosts");
-            Assert.assertTrue(hosts.contains("uk.bookinfo.com"));
-            Assert.assertTrue(hosts.contains("eu.bookinfo.com"));
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getHosts().size(), 2);
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getHosts().get(0), "uk.bookinfo.com");
+            Assert.assertEquals(gateway.getSpec().getServers().get(0).getHosts().get(1), "eu.bookinfo.com");
     
-            Map tls = (Map) server.get("tls");
-            Assert.assertEquals(true, tls.get("httpsRedirect"));
+            Assert.assertNotNull(gateway.getSpec().getServers().get(0).getTls());
+            Assert.assertTrue(gateway.getSpec().getServers().get(0).getTls().getHttpsRedirect());
     
-            yamlFile.deleteOnExit();
+            gwYaml.deleteOnExit();
         } catch (IOException e) {
             Assert.fail("Unable to write to file: " + e.getMessage());
         } catch (KubernetesPluginException e) {
