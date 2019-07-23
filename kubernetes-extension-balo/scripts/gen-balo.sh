ece8a5bed9d720b1537537ae683b1bad4c054add
Hemika Kodikara <hemika.kodikara@gmail.com>
2019-07-23 12:04:35 +0530

    update gen-balo.sh file with pushd check

--- kubernetes-extension-balo/scripts/gen-balo.sh
+++ kubernetes-extension-balo/scripts/gen-balo.sh
@@ -34,9 +34,16 @@ mkdir -p ${BALLERINAX_SYSTEM_LIB}
 
 rm -rf ${KUBERNETES_BALLERINA_PROJECT}/target
 
-pushd ${KUBERNETES_BALLERINA_PROJECT} /dev/null 2>&1
+if ! hash pushd 2>/dev/null
+then
+    cd ${KUBERNETES_BALLERINA_PROJECT}
     ${EXECUTABLE} compile --jvmTarget
-popd > /dev/null 2>&1
+    cd -
+else
+    pushd ${KUBERNETES_BALLERINA_PROJECT} /dev/null 2>&1
+        ${EXECUTABLE} compile --jvmTarget
+    popd > /dev/null 2>&1
+fi
 
 cp -r ${KUBERNETES_BALLERINA_PROJECT}/target/* ${KUBERNETES_BALO_MAVEN_PROJECT_ROOT}/target
 
