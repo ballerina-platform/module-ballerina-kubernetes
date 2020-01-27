10f2299dc7d063e6d31fe4ddd9ecf8362fa14b17
uvindu <uvindusri1996@gmail.com>
2020-01-27 17:11:35 +0530

    fix javadoc issue

--- kubernetes-extension-test/src/test/java/org/ballerinax/kubernetes/test/samples/Sample18Test.java
+++ kubernetes-extension-test/src/test/java/org/ballerinax/kubernetes/test/samples/Sample18Test.java
@@ -42,6 +42,9 @@ import static org.ballerinax.kubernetes.KubernetesConstants.DOCKER;
 import static org.ballerinax.kubernetes.KubernetesConstants.KUBERNETES;
 import static org.ballerinax.kubernetes.test.utils.KubernetesTestUtils.getExposedPorts;
 
+/**
+ * Test cases for sample 18.
+ */
 public class Sample18Test extends SampleTest {
 
     private static final Path SOURCE_DIR_PATH = SAMPLE_DIR.resolve("sample18");
